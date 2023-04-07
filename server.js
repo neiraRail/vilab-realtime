6aa1598966fb897041bc675fd10204e89a3b7210
Rail Neira <neira.rail@gmail.com>
2023-04-07 17:14:39 -0400

    Se agrega archivo server con prototipo de servidor

---
+++ server.js
@@ -0,0 +1,23 @@
+//server.js
+
+const express = require("express");
+const app = express();
+const port = 4000;
+
+const server = app.listen(`${port}`, function () {
+    console.log(`Server started on port ${port}`);
+});
+
+const io = require("socket.io")(server);
+
+function getRandomValue() {
+    return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+}
+//La vista vue envía un socker, dependiendo de qué nodo
+io.on("connection", socket => {
+    //El servidor conecta con mongo y comienza a enviar los datos tan rápido como es posible
+    setInterval(() => {
+        socket.broadcast.emit("newdata", getRandomValue())
+        console.log("enviado")
+    }, 5000)
+});
