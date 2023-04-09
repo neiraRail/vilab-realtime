56f17caecb36f99a8a46ac5473acba3381c42ecf
Rail Neira <neira.rail@gmail.com>
2023-04-09 01:18:39 -0400

    Agrega un evento 'realtime' que indica que nodo se va a escuchar, y usando ese nodo se crea el objeto pipeline que filtra el changeStream

--- server.js
+++ server.js
@@ -21,27 +21,31 @@ const io = new Server(server, {
 });
 
 //La vista vue envía un socker, dependiendo de qué nodo
-io.on("connection", async socket => {
+io.on("connection", socket => {
     console.log("Socket conectado")
     socket.on('disconnect', function () {
-        console.log('user disconnected');
+        console.log('Socket desconectado');
     });
-    try {
-        const database = client.db("vibration_db");
-        const haikus = database.collection("node");
+    socket.on('realtime', async (data) => {
+        console.log("pidiendo nodo: %d", data.node)
+        try {
+            const database = client.db("vibration_db");
+            const eventos = database.collection("event");
 
-        // Open a Change Stream on the "haikus" collection
-        changeStream = haikus.watch();
+            // Open a Change Stream on the "event" collection
+            const pipeline = [{ $match: { "fullDocument.node": data.node } }, { $match: { "operationType": "insert" } }]
+            changeStream = eventos.watch(pipeline);
 
-        // Print change events
-        for await (const change of changeStream) {
-            console.log("Received change:\n", change);
-            socket.broadcast.emit("newdata", change.fullDocument)
+            // Print change events
+            for await (const change of changeStream) {
+                console.log("Received change:\n", change);
+                socket.broadcast.emit("newdata", change.fullDocument)
+            }
+
+            await changeStream.close();
+
+        } finally {
+            await client.close();
         }
-
-        await changeStream.close();
-
-    } finally {
-        await client.close();
-    }
+    })
 });
