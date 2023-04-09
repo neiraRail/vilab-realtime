7a3012aa0f93dfb8e3deb3f17bcb33483b47dc67
Rail Neira <neira.rail@gmail.com>
2023-04-09 00:00:37 -0400

    Agrega ChangeStreams para escuchar a la base de datos y lo integra con socket.io

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 //server.js
+import { MongoClient } from "mongodb";
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
 
-const express = require("express");
+let changeStream;
+
+import express from "express"
 const app = express();
 const port = 4000;
 
@@ -8,20 +13,35 @@ const server = app.listen(`${port}`, function () {
     console.log(`Server started on port ${port}`);
 });
 
-const io = require("socket.io")(server, {
+import { Server } from "socket.io";
+const io = new Server(server, {
     cors: {
         origin: '*',
     }
 });
 
-function getRandomValue() {
-    return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
-}
 //La vista vue envía un socker, dependiendo de qué nodo
-io.on("connection", socket => {
-    //El servidor conecta con mongo y comienza a enviar los datos tan rápido como es posible
-    setInterval(() => {
-        socket.broadcast.emit("newdata", getRandomValue())
-        console.log("enviado")
-    }, 5000)
+io.on("connection", async socket => {
+    console.log("Socket conectado")
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+    try {
+        const database = client.db("vibration_db");
+        const haikus = database.collection("node");
+
+        // Open a Change Stream on the "haikus" collection
+        changeStream = haikus.watch();
+
+        // Print change events
+        for await (const change of changeStream) {
+            console.log("Received change:\n", change);
+            socket.broadcast.emit("newdata", change.fullDocument)
+        }
+
+        await changeStream.close();
+
+    } finally {
+        await client.close();
+    }
 });
