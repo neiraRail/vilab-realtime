19079239eb0439c7c272ec1a3435fc584105e37f
Rail Neira <r.neira03@ufromail.cl>
2023-12-06 22:56:04 -0300

    Agrega watch de features

---
+++ client.js
@@ -0,0 +1,10 @@
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:4000");
+socket.on("connect", () => {
+    console.log("Socket conectado");
+    socket.emit("feature", { node: 1 });
+});
+
+socket.on("newfeature", (data) => {
+    console.log("new feature: ", data)
+});
